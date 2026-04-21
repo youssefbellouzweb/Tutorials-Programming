957e63a9f38ac017cef8b9fe8559b74eead92b42
youssefbellouzweb <youssefbellouzweb@gmail.com>
2026-04-21 20:36:04 +0100

    added file lesson 03 about convert between types in c++

---
+++ C plus plus Tutorial/lesson03.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+
+    cout << (int)3.14 << endl; // output is 3
+    cout << (double)3 / 2 << endl; // output is 1.5
+
+    return 0;
+}
