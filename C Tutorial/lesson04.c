07d484655cdd0b26e9de7008fa4c91be835d5fef
youssefbellouzweb <youssefbellouzweb@gmail.com>
2026-04-21 14:45:11 +0100

    added file lesson 04 about Arithmatic Operator

---
+++ C Tutorial/lesson04.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/* 
+====Arithmatic Operator:====
+    Addition +
+    Subtraction -
+    Multiplication *
+    Division /
+    Remainder %
+
+    / * first
+    - + after
+*/
+
+void main() {
+    int x = 5;
+    int y = 2;
+    int result = x + y;
+
+    printf("x + y => %d \n", result);
+    printf("x + y => %d \n", x + y);
+    printf("x - y => %d \n", x - y);
+    printf("x * y => %d \n", x * y);
+    printf("x / y => %d \n", x / y);
+    printf("x %% y => %d \n", x % y); // %% in print %
+}
