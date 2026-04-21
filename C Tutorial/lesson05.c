e413a5ca541be6dd3b543949caf53c01e31d2ba0
youssefbellouzweb <youssefbellouzweb@gmail.com>
2026-04-21 14:58:53 +0100

    added file lesson 05 about if statement and both else if and else

--- C Tutorial/lesson05.c
+++ C Tutorial/lesson05.c
@@ -9,11 +9,19 @@
     !=
 */
 void main() {
-    int num = 5;
+    int num = 0;
 
     if (num == 5)
     {
-        printf("condition is true\n");
+        printf("%i condition is true\n", num);
+    } else if(num == 3) {
+        printf("condition is maybay 3\n");
+    } else if(num == 2) {
+        printf("condition is maybay 2\n");
+    } else if(num == 1) {
+        printf("condition is maybay 1\n");
+    } else
+    {
+        printf("condition is false %i\n", num);
     }
-    
 }
