d3f67b4f9d7b025c0ad1dc7f989bfd9fd05ed018
youssefbellouzweb <youssefbellouzweb@gmail.com>
2026-04-21 16:25:52 +0100

    added file lesson 11 about link between files

--- C Tutorial/lesson11.c
+++ C Tutorial/lesson11.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
+#include "my_functions.c"
 
 // int add(int num1, int num2)
 // {
 //     return num1 + num2;
 // }
-
-void add(int num1, int num2)
-{
-    printf("Result = %i \n", num1 + num2);
-}
 
 void main()
 {
---
+++ C Tutorial/my_functions.c
@@ -0,0 +1,4 @@
+void add(int num1, int num2)
+{
+    printf("Result = %i \n", num1 + num2);
+}
