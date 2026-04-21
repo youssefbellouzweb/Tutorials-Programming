f7fe2a2c8f421c479ddc94cba1908b70e0cb5c49
youssefbellouzweb <youssefbellouzweb@gmail.com>
2026-04-21 14:50:59 +0100

    added file lesson 05 about if statement

---
+++ C Tutorial/lesson05.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*
+--- conditions ---
+    >
+    <
+    >=
+    <=
+    ==
+    !=
+*/
+void main() {
+    int num = 5;
+
+    if (num == 5)
+    {
+        printf("condition is true\n");
+    }
+    
+}
