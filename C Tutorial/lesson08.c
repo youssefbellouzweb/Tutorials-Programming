e8132c6348429c8b41f4951731a812b59a75bd0e
youssefbellouzweb <youssefbellouzweb@gmail.com>
2026-04-21 15:53:50 +0100

    added file lesson 08 about for loop

---
+++ C Tutorial/lesson08.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main() {
+    for (int x = 0; x < 10 ; x++) {
+        printf("x = %i\n", x);
+    }
+
+    printf("==================\n");
+
+    for (int x = 10; x > 0 ; x--) {
+        printf("x = %i\n", x);
+    }
+}
