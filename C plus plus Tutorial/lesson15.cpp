3547809445d7e0ec585dfd59986dfd4697ce1ee6
youssefbellouzweb <youssefbellouzweb@gmail.com>
2026-04-22 17:31:14 +0100

    added file lesson 15 about do while loop in c++

---
+++ C plus plus Tutorial/lesson15.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int index = 1;
+
+    do
+    {
+        cout << index << endl;
+        index++;
+    } while (index <= 5);
+    return 0;
+}
