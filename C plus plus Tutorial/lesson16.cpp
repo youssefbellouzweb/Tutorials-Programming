31c8747fb436b0781a7206b5952c18c2fca6cabb
youssefbellouzweb <youssefbellouzweb@gmail.com>
2026-04-22 17:35:44 +0100

    added file lesson 16 about for loop in c++

---
+++ C plus plus Tutorial/lesson16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    const int count = 10;
+
+    for (int i = 0; i < count; i++)
+    {
+        cout << i << endl;
+    }
+    
+
+    return 0;
+}
