23368c6708a830eede669e8f69c627f2e98ef48e
youssefbellouzweb <youssefbellouzweb@gmail.com>
2026-04-22 15:39:46 +0100

    added file lesson 06 about number added more examples in c++

--- C plus plus Tutorial/lesson06.cpp
+++ C plus plus Tutorial/lesson06.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -9,10 +10,14 @@ int main()
     cout << 10 % 3 << endl;
     cout << (1 + 3) * 3 << endl;
 
+    cout << pow(2, 3) << endl;
+    cout << sqrt(2) << endl;
+    cout << round(2.3) << endl;
+
     int num = 10;
     num += 100;
     cout << num << endl;
-    
+
     num++;
     cout << num << endl;
 
