606d814c6d6e8c5b8d5f010f762ea5f588e04401
youssefbellouzweb <youssefbellouzweb@gmail.com>
2026-04-22 15:37:20 +0100

    added file lesson 06 about number in c++

---
+++ C plus plus Tutorial/lesson06.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    cout << 2 * 3 << endl;
+    cout << 10 % 3 << endl;
+    cout << (1 + 3) * 3 << endl;
+
+    int num = 10;
+    num += 100;
+    cout << num << endl;
+    
+    num++;
+    cout << num << endl;
+
+    return 0;
+}
